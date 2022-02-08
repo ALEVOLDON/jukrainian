d2478f6876c5310fca65576179e0670ca140afc0
Vladimir Rybalsky <alevoldon@gmail.com>
2022-02-08 21:42:10 +0300

    add burger menu

--- js/main.js
+++ js/main.js
@@ -42,5 +42,11 @@ function topFunction() {
 }
 
 // Mobile Navigation Menu
-
-
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
